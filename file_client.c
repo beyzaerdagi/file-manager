330e51151db593aa04c07e423e6299d902fad38a
Beyza <69843122+beyzaerdagi@users.noreply.github.com>
2023-01-02 22:34:21 +0300

    Update file_client.c

--- file_client.c
+++ file_client.c
@@ -9,42 +9,46 @@
 #include <unistd.h>
 #include <signal.h>
 
-int getIndex(char *data)
-{
-    int index = 0;
-    for (int i = 0; data[i] != '\0'; i++)
-    {
-        index++;
+//find input data length
+int findLength(char *data){
+    int len = 0;
+    for (int i = 0; data[i] != '\0'; i++){
+        len++;
     }
-    return index;
+    return len;
 }
 
-main()
-{
+main() {
     int fd;
 	int isExit = 1;
 	char *myfifo = "/tmp/myfifo";
 
 	mkfifo(myfifo, 0666);
     char response2[128];
-    char init[128] = "init";
+    //if client terminal is open
+    char client[128] = "client_created";
     fd = open(myfifo, O_WRONLY);
-    write(fd, init, sizeof(init));
+    write(fd, client, sizeof(client));
     close(fd);
-    while (isExit){
 
+    while (isExit){
         char data[128];
         char response[128];
+        //get input data from client
         fgets(data, 128, stdin);
-		if (data[getIndex(data) - 1] == '\n'){
-            data[getIndex(data) - 1] = '\0';
+        //delete space
+		if (data[findLength(data) - 1] == '\n'){
+            data[findLength(data) - 1] = '\0';
         }
+        //send manager input data
         fd = open(myfifo, O_WRONLY);
         write(fd, data, sizeof(data));
 		close(fd);
+        //if client enter exit, exit terminal
         if (strcmp(data, "exit") == 0){
 			isExit = 0;
 		}
+        //print response from manager
 		fd = open(myfifo, O_RDONLY);
 		read(fd, response, 128);
 		printf("%s\n", response);
